b21747a1cbff9b1963ea434f13d376b11e191b14
lee9805 <dlthwjd1161@naver.com>
2022-06-09 19:49:18 +0900

    project

---
+++ Team_project/src/main/java/com/callor/project/service/CategoryService.java
@@ -0,0 +1,10 @@
+package com.callor.project.service;
+
+import org.springframework.stereotype.Service;
+
+import com.callor.project.persistance.CategoryDao;
+
+@Service
+public interface CategoryService extends CategoryDao {
+	
+}
---
+++ Team_project/src/main/webapp/static/js/join.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const id_check = document.querySelector("button.id-check");
+  const join_btn = document.querySelector("button.join");
+  const join_form = document.querySelector("form.join");
+
+  const index = {
+    username: 0,
+    password: 1,
+    re_password: 2,
+    name: 3,
+    email: 4,
+  };
+  const inputList = document.querySelectorAll("form.join input");
+  const username = inputList[index.username];
+  const password = inputList[index.password];
+  const re_password = inputList[index.re_password];
+  const name = inputList[index.name];
+  const email = inputList[index.email];
+
+  const username_msg = document.querySelector("div.username");
+
+  const username_isBlank = () => {
+    username_msg.classList.remove("ok");
+    username_msg.classList.add("warn");
+    username_msg.innerText = " * USER NAME 을 입력해주세요";
+    username.focus();
+    return false;
+  };
+  if (join_btn) {
+    join_btn.addEventListener("click", () => {
+      if (username.value === "") {
+        username_isBlank();
+        return false;
+      }
+      if (re_password.value === "") {
+        alert("비밀번호 확인을 입력해 주세요");
+        re_password.focus();
+        return false;
+      }
+      if (password.value !== re_password.value) {
+        alert("비밀번화와 비밀번호 확인 값이 서로 다릅니다");
+        password.value = "";
+        re_password = "";
+        password.focus();
+        return false;
+      }
+      join_form.submit();
+    });
+  }
+
+  if (id_check) {
+    id_check.addEventListener("click", () => {
+      if (username.value === "") {
+        username_isBlank();
+        return false;
+      }
+      fetch(`${rootPath}/user/idcheck/${username.value}`)
+        .then((res) => {
+          console.table(index);
+          console.table(res);
+          return res.text();
+        })
+        .then((result) => {
+          if (result === "OK") {
+            username_msg.classList.remove("warn");
+            username_msg.classList.add("ok");
+            username_msg.innerText = " * 사용할수 있는 USER NAME입니다";
+          } else {
+            username_msg.classList.remove("ok");
+            username_msg.classList.add("warn");
+            username_msg.innerText = " * 이미 가입된 USER NAME입니다";
+          }
+        });
+    });
+  }
+});
